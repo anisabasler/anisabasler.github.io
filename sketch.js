ad73962c7ac37f2cbc9a901b15699157836c2c9f
anisabasler <104535614+anisabasler@users.noreply.github.com>
2022-05-22 11:39:39 -0400

    morestuff

--- sketch.js
+++ sketch.js
@@ -5,11 +5,14 @@ function setup() {
 function draw() {
 noStroke();
 fill(201, 30, 113);
-  ellipse(300,200,350,350);
-  
-fill(53, 130, 212);
-  text('Follow me on github ;)', 600, 200);
-  textSize(70,70);
+  ellipse(650,250,350,350);
+fill(255);
+ ellipse(560,160, 60, 60);
+ ellipse(680, 160, 60, 60);
+
+ fill(53, 130, 212);
+   text('Follow me on github! ;) @anisabasler  xoxo', 200, 50);
+   textSize(50);
 
 
 }
