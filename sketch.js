d7e3bc95ed820f47f2c130d89264bb08a65499b2
anisabasler <104535614+anisabasler@users.noreply.github.com>
2022-05-24 14:57:02 -0400

    colors

--- sketch.js
+++ sketch.js
@@ -5,14 +5,17 @@ function setup() {
 function draw() {
 noStroke();
 fill(201, 30, 113);
-  ellipse(650,250,350,350);
-fill(255);
- ellipse(560,160, 60, 60);
- ellipse(680, 160, 60, 60);
+  ellipse(650,350,350,350);
+fill(0);
+ ellipse(590,270, 60, 60);
+ ellipse(700, 270, 60, 60);
+ textSize(90);
+ text('3', 650,420);
 
  fill(53, 130, 212);
-   text('Follow me on github! ;) @anisabasler  xoxo', 200, 50);
-   textSize(50);
+ textSize(45,45);
+   text('Follow me on github! ;) @anisabasler  xoxo', 200, 100);
+
 
 
 }
